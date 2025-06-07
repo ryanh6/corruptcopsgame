e02954d94cb41e9d42c91dc0899ffd5c5ffee08d
Ryan Ho <ryanhohoho6@gmail.com>
2025-06-06 22:37:49 -0400

    wrote the outline of a few functions

--- corruptcops.cpp
+++ corruptcops.cpp
@@ -3,36 +3,42 @@
 #include <vector>
 
 class Player {
-	private:
-		int id;
-		bool corrupt;
-		std::string name;
-		std::string role;
-		std::string team;
-	public:
+	int id;
+	bool corrupt;
+	std::string name;
+	std::string role;
+	std::string team;
 };
 
-class Station {
-	private:
-		int id;
-		bool hasTreasure;
-		bool hasTrace;
-		std::vector<int> connections;
-	public:
+class Node {
+	int id;
+	bool hasTreasure;
+	bool hasTrace;
+	std::vector<int> connections;
 };
 
-class Map {
-	private:
-	public:
+class Board {
+	std::vector<Node> stations;
 };
 
 class CorruptCopsGame {
-	private:
-		int maxRounds;
-		std::vector<Player> playerList;
-		Map gameBoard;
+	int maxRounds;
+	std::vector<Player> playerList;
+	Board gameBoard;
+
 	public:
-		void play();
+		void play() {
+			// call assign roles
+			// make the game board (one line call, no function needed)
+			// placing pieces order
+			// actual rounds
+		}
+	private:
+		void assignRoles() {
+			// Shuffle the current player list
+			// Assign first 4 to blue, next to red, then to green, last 2 to thieves
+			// Choose 2 cops from different teams to be cops
+		}
 };
 
 int main() {
