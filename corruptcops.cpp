7158c31ce8faa69677c47feb454ef9e4997fb699
Ryan Ho <ryanhohoho6@gmail.com>
2025-06-08 00:58:06 -0400

    added functions for player and node class

--- corruptcops.cpp
+++ corruptcops.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <bits/stdc++.h>
 
 class Player {
 	int id;
@@ -8,13 +10,70 @@ class Player {
 	std::string name;
 	std::string role;
 	std::string team;
+
+	public:
+		Player(int newID, std::string newName) {
+			this->id = newID;
+			this->corrupt = false;
+			this->name = newName;
+			this->role = "None";
+			this->team = "None";
+		}
+		int getID() {
+			return this->id;
+		}
+		bool getCorrupt() {
+			return this->corrupt;
+		}
+		void setCorrupt() {
+			this->corrupt = !this->corrupt;
+		}
+		std::string getName() {
+			return this->name;
+		}
+		std::string getRole() {
+			return this->role;
+		}
+		void setRole(std::string newRole) {
+			this->role = newRole;
+		}
+		std::string getTeam() {
+			return this->team;
+		}
+		void setTeam(std::string newTeam) {
+			this->team = newTeam;
+		}
 };
 
 class Node {
-	int id;
+	int stationID;
 	bool hasTreasure;
 	bool hasTrace;
 	std::vector<int> connections;
+
+	public:
+		Node(int newID, std::vector<int> newConnections) {
+			this->stationID = newID;
+			this->hasTreasure = false;
+			this->hasTrace = false;
+			this->connections = newConnections;
+		}
+		int getID() {
+			return this->stationID;
+		}
+		bool getTreasure() {
+			return this->hasTreasure;
+		}
+		void setTreasure() {
+			this->hasTreasure = !this->hasTreasure;
+		}
+		bool getTrace() {
+			return this->hasTrace;
+		}
+		void setTrace() {
+			this->hasTrace = !this->hasTrace;
+		}
+
 };
 
 class Board {
@@ -22,7 +81,24 @@ class Board {
 
 	public:
 		void readMapLayout(std::string fileName) {
+			int count = 1;
+			std::string output;
+			std::ifstream myFile(fileName);
+
+			while (getline(myFile, output)) {
+				std::istringstream iss(output);
+
+				int value;
+				std::vector<int> numbers;
+
+				while (iss >> value) {
+					numbers.push_back(value);
+				}
 			
+				Node newStation = Node(count, numbers);
+				stations.push_back(newStation);
+				count++;
+			}
 		}
 };
 
